4f32d4baa0b57f161745db1a6d26d23575a344c9
Artyuischool <114410595+Artyuischool@users.noreply.github.com>
2023-01-24 09:31:09 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -15,7 +15,23 @@ input.onButtonPressed(Button.A, function () {
 })
 function apply_force (force: number, x: number, y: number) {
     if (force == 0) {
-    	
+        baly += -1
+    }
+    if (force == 1) {
+        if (!(balx == 0)) {
+            baly += -1
+            balx += -1
+        } else {
+            force = 2
+        }
+    }
+    if (force == 2) {
+        if (!(balx == 4)) {
+            baly += -1
+            balx += 1
+        } else {
+            force = 1
+        }
     }
 }
 function draw () {
@@ -32,6 +48,7 @@ function draw_net () {
     led.plot(2, 2)
     led.plot(3, 2)
     led.plot(4, 2)
+    log = "drawn net"
 }
 input.onButtonPressed(Button.B, function () {
     if (!(x == 4 || x > 4)) {
@@ -45,21 +62,25 @@ function draw_bal (x: number, y: number) {
         last_pos_bal_x = x
         last_pos_bal_y = y
     }
+    log = "drawn bal"
 }
 function punch () {
     led.plot(x, y - 1)
     basic.pause(150)
     led.unplot(x, y - 1)
     if (y - 1 == baly && x == balx) {
-        baly += -1
+        force = 0
     }
     if (y - 1 == baly && x - 1 == balx) {
-        baly += -1
+        force = 1
     }
     if (y - 1 == baly && x + 1 == balx) {
-        baly += -1
+        force = 2
     }
+    log = "punched"
 }
+let log = ""
+let force = 0
 let last_pos_bal_y = 0
 let last_pos_bal_x = 0
 let baly = 0
@@ -76,6 +97,22 @@ balx = 2
 baly = 3
 last_pos_bal_x = 2
 last_pos_bal_y = 3
+force = 1000
 basic.forever(function () {
     draw()
+})
+basic.forever(function () {
+    if (force == 0) {
+        apply_force(0, 1, 1)
+        log = "applied force 0"
+    }
+    if (force == 1) {
+        apply_force(1, 1, 1)
+        log = "applied force 1"
+    }
+    if (force == 2) {
+        apply_force(2, 1, 1)
+        log = "applied force 2"
+    }
+    basic.pause(100)
 })
